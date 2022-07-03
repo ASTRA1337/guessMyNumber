68559ce523c30fb1527cd9b32a9b57565773000f
Kody Smith <kwsmith90@gmail.com>
2022-07-03 13:42:50 -0500

    Refactoring

--- script.js
+++ script.js
@@ -7,12 +7,17 @@ let secretNumber = Math.trunc(Math.random()*20)+1;
 let score = 20;
 let highScore = 0;
 
+const displayMessage = function(message){
+    document.querySelector('.message').textContent = message;
+}
+
 
 
 document.querySelector('.again').addEventListener('click', function(){
     score = 20;
     secretNumber = Math.trunc(Math.random()*20)+1;
-    document.querySelector('.message').textContent = 'Please enter a Number!'
+    displayMessage('Please enter a Number!')
+    
     document.querySelector('.score').textContent = score;
     document.querySelector('.number').textContent = '?';
     document.querySelector('.guess').value = '';
@@ -27,7 +32,7 @@ document.querySelector('.again').addEventListener('click', function(){
 document.querySelector('.check').addEventListener('click', function(){
   let guess = Number(document.querySelector('.guess').value);
   if(!guess) {
-      document.querySelector('.message').textContent = 'Please enter a Number!';
+      displayMessage('Please enter a Number!');
     } else if(guess === secretNumber){
         document.querySelector('.message').textContent = 'Correct Number!';
         document.querySelector('.number').textContent = secretNumber;
@@ -46,7 +51,7 @@ document.querySelector('.check').addEventListener('click', function(){
             document.querySelector('.score').textContent = score;
 
         }else{
-            document.querySelector('.message').textContent = '🤯 You Lost the Game!';
+            displayMessage('🤯 You Lost the Game!');
             document.querySelector('.score').textContent = 0;
         }
         
