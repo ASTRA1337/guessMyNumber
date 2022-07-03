7f1581fdfbfaad90d596dd15d4590f4916e79e29
Kody Smith <kwsmith90@gmail.com>
2022-07-03 13:47:52 -0500

    Refactoring

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ document.querySelector('.check').addEventListener('click', function(){
 
     }else if( guess !== secretNumber){
         if(score > 1){
-            document.querySelector('.message').textContent = guess > secretNumber ? 'Too High!' : 'Too Low!';
+            displayMessage(guess > secretNumber ? 'Too High!' : 'Too Low!');
             score--;
             document.querySelector('.score').textContent = score;
 
